fb86e242995cd864572d768fedee0015ee17d10d
Dylan Havelock <dylan.havelock@gmail.com>
2020-11-11 21:29:19 -0500

    typo

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/master-pool-ui'));
+app.use(express.static(__dirname + '/dist/masters-pool-ui'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/master-pool-ui/index.html'));
+res.sendFile(path.join(__dirname+'/dist/masters-pool-ui/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
